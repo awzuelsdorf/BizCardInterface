2112499b705b32d21848885842e03112c680bffd
Richard Johnson <rj7315445@gmail.com>
2018-12-20 21:06:55 -0500

    Added more challenging business card cases, implemented email parsing.

--- BusinessCardParser.py
+++ BusinessCardParser.py
@@ -25,8 +25,18 @@ class BusinessCardParser(object):
         return mostLikelyName
 
     @classmethod
-    def getMostLikelyPhone(cls, document):
-        possiblePhones = re.findall(r"\s*(?:\+?(\d{1,3}))?[-.\s(]*(\d{3})[-.\s)]*(\d{3})[-.\s]*(\d{4})(?:\s*(ex|x|ext|extension)\s*(\d+))?\s*", document, flags=re.I)
+    def getMostLikelyPhone(cls, document, extension=False):
+        possiblePhones = list()
+
+        if extension:
+            regex = r"(?:\+?(\d{1,3}))?[-.\s(]*(\d{3})[-.\s)]*(\d{3})[-.\s]*(\d{4})(?:\s*(ex|x|ext|extension)\s*(\d+))?"
+        else:
+            regex = r"(?:\+?(\d{1,3}))?[-.\s(]*(\d{3})[-.\s)]*(\d{3})[-.\s]*(\d{4})?"
+
+        for line in re.split(r"\n", document):
+            #Skip fax machine numbers.
+            if "f" not in line.lower():
+                possiblePhones.extend(re.findall(regex, line, flags=re.I))
 
         if len(possiblePhones) > 0:
             return "".join(possiblePhones[0])
@@ -34,10 +44,24 @@ class BusinessCardParser(object):
             return None
 
     @classmethod
+    def getMostLikelyEmail(cls, document):
+        possibleEmails = list()
+
+        for line in re.split(r"\n", document):
+            possibleEmails.extend(re.findall(r"\s*([^@\s]+@\S+)",
+line, flags=re.I))
+
+        if len(possibleEmails) > 0:
+            return "".join(possibleEmails[0])
+        else:
+            return None
+
+    @classmethod
     def getContactInfo(cls, document):
         mostLikelyName = cls.getMostLikelyName(document)
         mostLikelyPhone = cls.getMostLikelyPhone(document)
+        mostLikelyEmail = cls.getMostLikelyEmail(document)
 
-        info = ContactInfo(mostLikelyName, mostLikelyPhone, "andrew@andrew.com")
+        info = ContactInfo(mostLikelyName, mostLikelyPhone, mostLikelyEmail)
 
         return info
